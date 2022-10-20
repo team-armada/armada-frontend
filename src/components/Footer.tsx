8eb3707373db015febc3244366d62bcba135d58b
Joey <josephdguillaume@gmail.com>
2022-10-20 15:11:13 -0400

    Updated footer year to utilize lazy initialization.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
+
 const Footer = () => {
-  const today = new Date();
-  return <footer>&copy;{` ${today.getFullYear()} Armada Team`}</footer>;
+  const getCurrentYear = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    return year;
+  };
+
+  const [year, setYear] = useState(getCurrentYear);
+
+  return <footer>&copy;{` ${year} Armada Team`}</footer>;
 };
 
 export default Footer;
