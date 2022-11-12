049b54ea321b6ad14eae5e507265674b22c021bb
Joey <josephdguillaume@gmail.com>
2022-11-12 11:21:52 -0500

    Added vite proxy path to resolve calls to backend in development.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    "global":  {},
+    global: {},
   },
-  plugins: [react()]
-})
+
+  server: {
+    host: true,
+    proxy: {
+      '/cohort': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/course': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/service': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/template': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/user': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/auth': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  plugins: [react()],
+});
